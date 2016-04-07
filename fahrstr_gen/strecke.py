bd88cd8597f1eb2ecc940e8ef6a1dfaf0f4e9ae8
Johannes <zusitools@web.de>
2016-04-07 22:27:59 +0200

    Entferne TODO in strecke._escape-Funktion

--- fahrstr_gen/strecke.py
+++ fahrstr_gen/strecke.py
@@ -488,7 +488,7 @@ st3_attrib_order = {
 }
 
 def _escape(txt):
-    # TODO: das ist nicht die beste Escape-Funktion, aber Zusi macht auch nicht viel mehr.
+    # Nicht die beste Escape-Funktion, aber Zusi macht auch nicht viel mehr.
     # Zur Performance siehe http://stackoverflow.com/a/27086669/1083696 (die Strings in Zusi-XML-Dateien sind eher kurz)
     if "&" in txt:
         txt = txt.replace("&", "&amp;")
