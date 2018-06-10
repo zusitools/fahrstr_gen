05b22996b07d8e44ee671645ad1617d77c38cb58
Johannes <zusitools@web.de>
2018-06-10 18:37:55 +0200

    Ausgabe: Schreibe einheitlich 'g' und 'b' fuer Norm- und Gegenrichtung

--- fahrstr_gen/modulverwaltung.py
+++ fahrstr_gen/modulverwaltung.py
@@ -76,7 +76,7 @@ class RefPunkt(object):
         global dieses_modul
         return "{}{}{}".format(
             self.element_richtung.element.xml_knoten.get("Nr", "0"),
-            'n' if self.element_richtung.richtung == NORM else 'g',
+            'b' if self.element_richtung.richtung == NORM else 'g',
             "" if self.element_richtung.element.modul == dieses_modul else "[{}]".format(self.modul_kurz())
         )
 
